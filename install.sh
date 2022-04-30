9a1081d57acd04a512c4cd43f307bf79c3aeb859
Yaya-Cout <yaya.cout@free.fr>
2022-04-30 14:56:18 +0200

    [installer] Config /etc

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
+# Include helper functions
+source "shell/helpers.sh"
 # Get path
 SHELLCONFPATH=$(pwd)
+# Enable debugging
+export DEBUG=1
 # Configure the given rc file shell
 function config_shell() {
   SHELLPATH="$1"
@@ -16,19 +20,74 @@ function config_shell() {
     echo "${RCCONF}" >> "${SHELLPATH}"
   fi
 }
-config_shell "${HOME}/.zshrc"
-config_shell "${HOME}/.bashrc"
-# Get if ~/.profile exists
-if [[ -f ~/.profile ]]; then
-    # Get if ~/.profile is a hard link by checking if the file has more than one name
-    if [[ "$(stat -c %h -- ~/.profile)" -gt 1 ]]; then
-        echo "~/.profile is already configured"
-    # Else ~/.profile isn't configured
+# Configure .profile
+function config_profile() {
+  # Get if ~/.profile exists
+  if [[ -f ~/.profile ]]; then
+      # Get if ~/.profile is a hard link by checking if the file has more than one name
+      if is_hard_link ~/.profile; then
+          echo "~/.profile is already configured"
+      # Else ~/.profile isn't configured
+      else
+          echo "Configuring ~/.profile"
+          # Backup ~/.profile
+          backup_file ~/.profile
+          # Hard link ~/.profile to $SHELLCONFPATH/shell/common/profile.sh
+          ln "${SHELLCONFPATH}/shell/common/profile.sh" ~/.profile
+      fi
+  fi
+}
+# Configure /etc
+function config_etc() {
+  # Get the argument
+  path_to_config=$1
+  echo "Configuring ${path_to_config}"
+  # Iterate over all files in ./etc
+  for file in ".${path_to_config}/"*; do
+    # Remove the first dot from the file name
+    file_name=${file:1}
+    # Get if file is a directory
+    if [[ -d ${file} ]]; then
+      # Call this function recursively
+      echo "Entering directory: ${file_name}"
+      config_etc "${file_name}"
+    # Else, it is a file
     else
-        echo "Configuring ~/.profile"
-        # Backup ~/.profile
-        mv ~/.profile ~/.profile.bak
-        # Hard link ~/.profile to $SHELLCONFPATH/shell/common/profile.sh
-        ln "${SHELLCONFPATH}/shell/common/profile.sh" ~/.profile
+      # Get if file is a hard link by checking if the file has more than one name
+      if is_hard_link "${file}"; then
+        echo "${file_name} is already configured"
+      # Else file isn't configured
+      else
+        echo "Configuring ${file_name} (hard link from ${file} to ${file_name})"
+        # Backup file if it exists
+        if [[ -f "${file_name}" ]]; then
+          backup_file "${file_name}"
+        fi
+        # Hard link file to $SHELLCONFPATH/shell/common/etc/file
+        ln "${file}" "${file_name}"
+      fi
     fi
+  done
+}
+# Handle special configs
+function handle_special_configs() {
+  # Handle /etc/fstab
+  append_lines_to_file "/etc/fstab" "special_parsing_config/fstab"
+  # Update the grub config
+  echo "Updating grub config"
+  update-grub
+}
+# If the user is not root, configure the shell
+if [[ $EUID -ne 0 ]]; then
+  echo "User is not root, configuring shell"
+  config_shell "${HOME}/.zshrc"
+  config_shell "${HOME}/.bashrc"
+  config_profile
+# Else, the user is root
+else
+  echo "User is root, configuring global configs"
+  # Configure /etc
+  config_etc "/etc"
+  # Handle special configs
+  handle_special_configs
 fi
--- shell/helpers.sh
+++ shell/helpers.sh
@@ -53,3 +53,63 @@ function source_dir() {
     fi
   done
 }
+# Get if hard link exists
+function is_hard_link() {
+  # Initialize function arguments
+  local file=$1
+  # Get if file exists
+  if [[ -f ${file} ]]; then
+    # Get if file is a hard link by checking if the file has more than one name
+    if [[ "$(stat -c %h -- ${file})" -gt 1 ]]; then
+      return 0
+    else
+      return 1
+    fi
+  else
+    return 1
+  fi
+}
+
+# Backup a file if it exists
+function backup_file() {
+  # Initialize function arguments
+  local file=$1
+  # Get if file exists
+  if [[ -f ${file} ]]; then
+    # Log the file
+    if [[ ${DEBUG} -eq 1 ]]; then
+      echo "Backing up ${file}"
+    fi
+    # Backup the file
+    mv "${file}" "${file}.bak"
+  fi
+}
+
+# Append lines from a file to another file, if some lines are already in the file, don't append them
+# @param $1: The file to append to
+# @param $2: The file to append from
+function append_lines_to_file() {
+  # Initialize function arguments
+  local output_file=$1
+  local input_file=$2
+  # Log the function
+  if [[ ${DEBUG} -eq 1 ]]; then
+    echo "Importing lines from ${input_file} to ${output_file}"
+    echo "Backup file: ${output_file}.bak"
+  fi
+  # Backup the file
+  backup_file "${output_file}"
+  # Get if destination file exists
+  if [[ -f ${output_file} ]]; then
+      # Get the lines from the output file
+      output_file_lines=$(cat "${output_file}")
+      # Iterate over all lines in the file using a while loop
+      while read -r line; do
+        # If line is not in the file, append it
+        if [[ ! "${output_file_lines}" =~ "${line}" ]]; then
+          echo "Appending line: ${line}"
+          echo "${line}" >> "${output_file}"
+        fi
+      done < "${input_file}"
+  fi
+}
