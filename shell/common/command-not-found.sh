60c874b558b8bc8ac64ba30234acb4e28f49b195
Yaya-Cout <yaya.cout@free.fr>
2022-08-10 10:38:37 +0200

    [shell/common/command-not-found] Add command not found

---
+++ shell/common/command-not-found.sh
@@ -0,0 +1,5 @@
+# Enable find-the-command
+source_if_exists /usr/share/doc/find-the-command/ftc.zsh 2>/dev/null
+# Enable command-not-found
+source_if_exists /usr/share/doc/pkgfile/command-not-found.bash
+source_if_exists /usr/share/doc/pkgfile/command-not-found.zsh
